c33a1a38e3a7bbbb9a63b394028129881a6a5122
kazuma4g <93083318+kazuma4g@users.noreply.github.com>
2021-11-28 04:44:42 -0500

    値、変数、構文をマスターする
    Chapter 2

---
+++ node/app/test1/test1.js
@@ -0,0 +1,12 @@
+var anExample = "Hello, World!";
+console.log(anExample);
+/*
+let x:number
+x = 123
+console.log(x)
+*/
+var x = 123;
+console.log(x);
+var y = "456";
+x = +y;
+console.log(x);
---
+++ node/app/test1/test1.ts
@@ -0,0 +1,13 @@
+const anExample = "Hello, World!"
+console.log(anExample)
+
+/*
+let x:number
+x = 123
+console.log(x)
+*/
+let x = 123
+console.log(x)
+let y = "456"
+x = +y
+console.log(x)
